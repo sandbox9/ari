de169c4baf9f2a925e78ab2583da5716324478f6
poets11 <poets11@gmail.com>
2014-12-15 17:02:55 +0900

    GSShop 장바구니 페이지 마크업적용 및 컨트롤러 신규생성 DemoSite의 레이아웃이 적용되지 않도록 thymeleaf설정 수정

---
+++ site/src/main/java/com/ari/controller/gsshop/cart/CartController.java
@@ -0,0 +1,21 @@
+package com.ari.controller.gsshop.cart;
+
+import org.broadleafcommerce.core.web.controller.cart.BroadleafCartController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by poets11 on 14. 12. 15..
+ */
+@Controller
+@RequestMapping(value = "/gsshop/cart")
+public class CartController extends BroadleafCartController {
+    @RequestMapping(value = "/list")
+    public String test(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "gsshop/cart/list";
+    }
+}
