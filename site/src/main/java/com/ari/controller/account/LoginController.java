f647da39ddfda7b27bf0a091e4b3f6901edc19cf
Dennis,Koo <gwkoo@itwise.co.kr>
2014-12-18 16:49:11 +0900

    로그인 페이지 변경

--- site/src/main/java/com/ari/controller/account/LoginController.java
+++ site/src/main/java/com/ari/controller/account/LoginController.java
@@ -16,6 +16,9 @@
 
 package com.ari.controller.account;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.broadleafcommerce.common.exception.ServiceException;
 import org.broadleafcommerce.core.web.controller.account.BroadleafLoginController;
 import org.broadleafcommerce.core.web.controller.account.ResetPasswordForm;
@@ -27,49 +30,71 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * The controller responsible for all actions involving logging a customer in
  */
 @Controller
 public class LoginController extends BroadleafLoginController {
-    
-    @RequestMapping("/login")
-    public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
-        return super.login(request, response, model);
-    }
-    
-    @RequestMapping(value="/login/forgotPassword", method=RequestMethod.GET)
-    public String forgotPassword(HttpServletRequest request, HttpServletResponse response, Model model) {
-        return super.forgotPassword(request, response, model);
-    }
-    
-    @RequestMapping(value="/login/forgotPassword", method=RequestMethod.POST)
-    public String processForgotPassword(@RequestParam("emailAddress") String emailAddress, HttpServletRequest request, Model model) {
-        return super.processForgotPassword(emailAddress, request, model);
-    }   
 
-    @RequestMapping(value="/login/resetPassword", method=RequestMethod.GET)
-    public String resetPassword(HttpServletRequest request, HttpServletResponse response, Model model) {
-        return super.resetPassword(request, response, model);
-    }   
-    
-    @RequestMapping(value="/login/resetPassword", method=RequestMethod.POST)
-    public String processResetPassword(@ModelAttribute("resetPasswordForm") ResetPasswordForm resetPasswordForm, HttpServletRequest request, HttpServletResponse response, Model model, BindingResult errors) throws ServiceException {
-        return super.processResetPassword(resetPasswordForm, request, response, model, errors);
-    }   
-    
-    @Override
-    public String getResetPasswordUrl(HttpServletRequest request) {     
-        String url = request.getScheme() + "://" + request.getServerName() + getResetPasswordPort(request, request.getScheme() + "/");
-        
-        if (request.getContextPath() != null && ! "".equals(request.getContextPath())) {
-            url = url + request.getContextPath() + "/login/resetPassword";
-        } else {
-            url = url + "/login/resetPassword";
-        }
-        return url;
-    }
+	private static String loginView = "ari/authentication/login";
+
+	@Override
+	@RequestMapping("/login")
+	public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
+		super.login(request, response, model);
+		return getLoginView();
+	}
+
+	@Override
+	@RequestMapping(value = "/login/forgotPassword", method = RequestMethod.GET)
+	public String forgotPassword(HttpServletRequest request, HttpServletResponse response, Model model) {
+		return super.forgotPassword(request, response, model);
+	}
+
+	@Override
+	@RequestMapping(value = "/login/forgotPassword", method = RequestMethod.POST)
+	public String processForgotPassword(@RequestParam("emailAddress") String emailAddress, HttpServletRequest request, Model model) {
+		return super.processForgotPassword(emailAddress, request, model);
+	}
+
+	@Override
+	@RequestMapping(value = "/login/resetPassword", method = RequestMethod.GET)
+	public String resetPassword(HttpServletRequest request, HttpServletResponse response, Model model) {
+		return super.resetPassword(request, response, model);
+	}
+
+	@Override
+	@RequestMapping(value = "/login/resetPassword", method = RequestMethod.POST)
+	public String processResetPassword(@ModelAttribute("resetPasswordForm") ResetPasswordForm resetPasswordForm, HttpServletRequest request,
+			HttpServletResponse response, Model model, BindingResult errors) throws ServiceException {
+		return super.processResetPassword(resetPasswordForm, request, response, model, errors);
+	}
+
+	@Override
+	public String getResetPasswordUrl(HttpServletRequest request) {
+		String url = request.getScheme() + "://" + request.getServerName() + getResetPasswordPort(request, request.getScheme() + "/");
+
+		if (request.getContextPath() != null && !"".equals(request.getContextPath())) {
+			url = url + request.getContextPath() + "/login/resetPassword";
+		} else {
+			url = url + "/login/resetPassword";
+		}
+		return url;
+	}
+
+	/**
+	 * @return the loginView
+	 */
+	@Override
+	public String getLoginView() {
+		return loginView;
+	}
+
+	/**
+	 * @param loginView
+	 *            the loginView to set
+	 */
+	public void setLoginView(String loginView) {
+		LoginController.loginView = loginView;
+	}
 }
