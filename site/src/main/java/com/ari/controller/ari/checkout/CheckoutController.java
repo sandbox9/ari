0e57a833f4f68e36d688b3cdb3c41bc283b78daf
poets11 <poets11@gmail.com>
2014-12-23 10:59:16 +0900

    결제 페이지 수정

--- site/src/main/java/com/ari/controller/ari/checkout/CheckoutController.java
+++ site/src/main/java/com/ari/controller/ari/checkout/CheckoutController.java
@@ -3,18 +3,26 @@ package com.ari.controller.ari.checkout;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.broadleafcommerce.common.exception.ServiceException;
+import org.broadleafcommerce.common.payment.PaymentType;
+import org.broadleafcommerce.common.vendor.service.exception.PaymentException;
+import org.broadleafcommerce.core.pricing.service.exception.PricingException;
 import org.broadleafcommerce.core.web.checkout.model.BillingInfoForm;
 import org.broadleafcommerce.core.web.checkout.model.CustomerCreditInfoForm;
 import org.broadleafcommerce.core.web.checkout.model.GiftCardInfoForm;
 import org.broadleafcommerce.core.web.checkout.model.OrderInfoForm;
 import org.broadleafcommerce.core.web.checkout.model.ShippingInfoForm;
 import org.broadleafcommerce.core.web.controller.checkout.BroadleafCheckoutController;
+import org.broadleafcommerce.profile.core.domain.Customer;
+import org.broadleafcommerce.profile.web.core.CustomerState;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -29,29 +37,33 @@ public class CheckoutController extends BroadleafCheckoutController {
             @ModelAttribute("customerCreditInfoForm") CustomerCreditInfoForm customerCreditInfoForm,
             RedirectAttributes redirectAttributes) {
     	
-//    	super.checkout(request, response, model, redirectAttributes);
+    	super.checkout(request, response, model, redirectAttributes);
+    	
+    	Customer customer = CustomerState.getCustomer();
+    	model.addAttribute("customer", customer);
+    	
     	return "ari/order/checkout";
     }
 
-//    @RequestMapping(value = "/checkout/savedetails", method = RequestMethod.POST)
-//    public String saveGlobalOrderDetails(HttpServletRequest request, Model model,
-//            @ModelAttribute("shippingInfoForm") ShippingInfoForm shippingForm,
-//            @ModelAttribute("billingInfoForm") BillingInfoForm billingForm,
-//            @ModelAttribute("giftCardInfoForm") GiftCardInfoForm giftCardInfoForm,
-//            @ModelAttribute("orderInfoForm") OrderInfoForm orderInfoForm, BindingResult result) throws ServiceException {
-//        return super.saveGlobalOrderDetails(request, model, orderInfoForm, result);
-//    }
-//
-//    @RequestMapping(value = "/checkout/complete", method = RequestMethod.POST)
-//    public String processCompleteCheckoutOrderFinalized(RedirectAttributes redirectAttributes) throws PaymentException {
-//        return super.processCompleteCheckoutOrderFinalized(redirectAttributes);
-//    }
-//
-//    @RequestMapping(value = "/checkout/cod/complete", method = RequestMethod.POST)
-//    public String processPassthroughCheckout(RedirectAttributes redirectAttributes)
-//            throws PaymentException, PricingException {
-//        return super.processPassthroughCheckout(redirectAttributes, PaymentType.COD);
-//    }
+    @RequestMapping(value = "/checkout/savedetails", method = RequestMethod.POST)
+    public String saveGlobalOrderDetails(HttpServletRequest request, Model model,
+            @ModelAttribute("shippingInfoForm") ShippingInfoForm shippingForm,
+            @ModelAttribute("billingInfoForm") BillingInfoForm billingForm,
+            @ModelAttribute("giftCardInfoForm") GiftCardInfoForm giftCardInfoForm,
+            @ModelAttribute("orderInfoForm") OrderInfoForm orderInfoForm, BindingResult result) throws ServiceException {
+        return super.saveGlobalOrderDetails(request, model, orderInfoForm, result);
+    }
+
+    @RequestMapping(value = "/checkout/complete", method = RequestMethod.POST)
+    public String processCompleteCheckoutOrderFinalized(RedirectAttributes redirectAttributes) throws PaymentException {
+        return super.processCompleteCheckoutOrderFinalized(redirectAttributes);
+    }
+
+    @RequestMapping(value = "/checkout/cod/complete", method = RequestMethod.POST)
+    public String processPassthroughCheckout(RedirectAttributes redirectAttributes)
+            throws PaymentException, PricingException {
+        return super.processPassthroughCheckout(redirectAttributes, PaymentType.COD);
+    }
 
     @InitBinder
     protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
